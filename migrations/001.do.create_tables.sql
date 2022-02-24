ba34e38587e473141b56f6b4ad8ae688dc21c147
Darcey Grundy <grundy.darcey@gmail.com>
2022-02-23 21:01:08 -0500

    adding picture to table

--- migrations/001.do.create_tables.sql
+++ migrations/001.do.create_tables.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS members (
     id INT GENERATED ALWAYS AS IDENTITY,
     member_name TEXT NOT NULL,
     member_partner TEXT,
-    price INTEGER NOT NULL
+    price INTEGER NOT NULL,
+    member_picture TEXT NOT NULL
 );
