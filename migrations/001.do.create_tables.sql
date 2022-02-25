b3e30e5694b3feb406153c934b70ede379c8afab
Darcey Grundy <grundy.darcey@gmail.com>
2022-02-24 21:42:50 -0500

    added personal lists table and seeded data

--- migrations/001.do.create_tables.sql
+++ migrations/001.do.create_tables.sql
@@ -1,7 +1,17 @@
 CREATE TABLE IF NOT EXISTS members (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
     member_name TEXT NOT NULL,
     member_partner TEXT,
     price INTEGER NOT NULL,
-    member_picture TEXT NOT NULL
+    member_picture TEXT NOT NULL,
+    group_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS  personalLists (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    users_id INT NOT NULL,
+    link TEXT NOT NULL,
+    FOREIGN KEY (users_id)
+      REFERENCES members(id)
+      ON DELETE CASCADE
 );
